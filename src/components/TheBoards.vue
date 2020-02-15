4ef440cd72205654fb7c7e2aaa795a3d3e482ce1
x-color <36035885+x-color@users.noreply.github.com>
2020-02-15 12:45:22 +0900

    Add Boards component

---
+++ src/components/TheBoards.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container>
+    <!-- PIN: Boards -->
+    <draggable
+      class="row flex-nowrap row--dense justify-start"
+      group="boards"
+      v-model="boards"
+      draggable=".item"
+    >
+      <v-col v-for="(board, i) in boards" :key="i" cols="auto" class="item">
+        <v-card dark>
+          <router-link :to="`/boards/${board.id}`" class="router-no-underline">
+            <CardBoard :id="board.id" />
+          </router-link>
+        </v-card>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          class="mx-auto"
+          width="300"
+          height="100"
+          color="grey lighten-2"
+          @click="addBoardMode = true"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-col>
+    </draggable>
+
+    <!-- PIN: Modal for creating new board -->
+    <ModalBoard
+      v-model="newBoard"
+      :open="addBoardMode"
+      @close="resetNewBoard"
+      @save="addNewBoard"
+    />
+  </v-container>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import draggable from 'vuedraggable';
+import ModalBoard from '@/components/ModalBoard.vue';
+import CardBoard from '@/components/CardBoard.vue';
+
+export default {
+  name: 'TheBoards',
+  components: {
+    CardBoard,
+    ModalBoard,
+    draggable,
+  },
+  computed: {
+    ...mapGetters(['getBoardsByUserId', 'user']),
+    boards: {
+      get() {
+        return this.getBoardsByUserId;
+      },
+      set(newValue) {
+        const newUser = Object.assign(
+          { ...this.user },
+          { boards: newValue.map(board => board.id) },
+        );
+        this.editUser(newUser);
+      },
+    },
+  },
+  data() {
+    return {
+      addBoardMode: false,
+      newBoard: { title: '', text: '', color: 'indigo' },
+    };
+  },
+  methods: {
+    ...mapActions(['addBoard', 'editUser']),
+    addNewBoard() {
+      this.addBoard({
+        userId: this.user.id,
+        color: this.newBoard.color,
+        text: this.newBoard.text,
+        title: this.newBoard.title,
+      });
+      this.resetNewBoard();
+    },
+    resetNewBoard() {
+      this.newBoard = { title: '', text: '', color: 'indigo' };
+      this.addBoardMode = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.router-no-underline {
+  text-decoration: none;
+}
+</style>
