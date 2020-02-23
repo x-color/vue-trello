5947497025d82901e4b1e4c154fbce066815980a
x-color <36035885+x-color@users.noreply.github.com>
2020-02-23 20:24:17 +0900

    Add tags and colors usecase

---
+++ usecase/resources.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"github.com/x-color/vue-trello/model"
+)
+
+// ResourseUsecase is interface. It defines getter for tags and colors.
+type ResourseUsecase interface {
+	GetAllTagsandColors() (model.Tags, model.Colors, error)
+}
+
+// ResourceInteractor includes repogitories and a logger.
+type ResourceInteractor struct {
+	tagRepo TagRepogitory
+	logger  Logger
+}
+
+// NewResourceInteractor generates new interactor for resources.
+func NewResourceInteractor(
+	tagRepo TagRepogitory,
+	logger Logger,
+) (ResourceInteractor, error) {
+	i := ResourceInteractor{
+		tagRepo: tagRepo,
+		logger:  logger,
+	}
+	return i, nil
+}
+
+// GetAllTagsandColors returns all Tags and Colors.
+func (i *ResourceInteractor) GetAllTagsandColors() (model.Tags, model.Colors, error) {
+	tags, err := i.tagRepo.FindAll()
+	if err != nil {
+		return model.Tags{}, model.Colors{}, err
+	}
+	return tags, model.COLORS, nil
+}
