6fd9fd953cb9e504e1337c8dea63d6baeda00e62
x-color <36035885+x-color@users.noreply.github.com>
2020-02-23 19:42:21 +0900

    Move errros to model
    I forgot to add errors.go to model package in this commit(5bf3d590617d78702cdae5823b35596f3a046508)

---
+++ model/errors.go
@@ -0,0 +1,51 @@
+package model
+
+import "fmt"
+
+// NotFoundError is occured if a content is not in a repogitory.
+type NotFoundError struct {
+	ID  string
+	Act string
+	Err error
+}
+
+func (e NotFoundError) Error() string {
+	return fmt.Sprintf("%s: %s does not exist", e.Act, e.ID)
+}
+
+// Unwrap returns a error wrapped by NotFoundError.
+func (e NotFoundError) Unwrap() error {
+	return e.Err
+}
+
+// InvalidContentError is occured if a content is invalid.
+type InvalidContentError struct {
+	ID  string
+	Act string
+	Err error
+}
+
+func (e InvalidContentError) Error() string {
+	return fmt.Sprintf("%s: %s has invalid content", e.Act, e.ID)
+}
+
+// Unwrap returns a error wrapped by InvalidContentError.
+func (e InvalidContentError) Unwrap() error {
+	return e.Err
+}
+
+// ServerError is occured if a error is occured in server and not bad request.
+type ServerError struct {
+	ID  string
+	Act string
+	Err error
+}
+
+func (e ServerError) Error() string {
+	return fmt.Sprintf("%s: %s. internal server error is occured", e.Act, e.ID)
+}
+
+// Unwrap returns a error wrapped by ServerError.
+func (e ServerError) Unwrap() error {
+	return e.Err
+}
