93af707b11c652ac4ed26b062a07542a7bf92307
x-color <36035885+x-color@users.noreply.github.com>
2020-02-25 00:59:12 +0900

    Add http handler to control board

---
+++ interface/controller/api/handler/board.go
@@ -0,0 +1,153 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/x-color/vue-trello/model"
+	"github.com/x-color/vue-trello/usecase"
+)
+
+// Board includes request data for Board.
+type Board struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+	Lists []List `json:"lists"`
+	Color string `json:"color"`
+}
+
+func (b *Board) convertTo() model.Board {
+	board := model.Board{
+		ID:    b.ID,
+		Title: b.Title,
+		Text:  b.Text,
+		Color: model.Color(b.Color),
+	}
+
+	return board
+}
+
+func (b *Board) convertFrom(board model.Board) {
+	b.ID = board.ID
+	b.Title = board.Title
+	b.Text = board.Text
+	b.Color = string(board.Color)
+	lists := []List{}
+	for _, i := range board.Lists {
+		list := List{}
+		list.convertFrom(i)
+		lists = append(lists, list)
+	}
+	b.Lists = lists
+}
+
+// BoardHandler includes a interactor for Board usecase.
+type BoardHandler struct {
+	intractor usecase.BoardUsecase
+}
+
+// NewBoardHandler returns a new BoardHandler.
+func NewBoardHandler(i usecase.BoardUsecase) *BoardHandler {
+	return &BoardHandler{
+		intractor: i,
+	}
+}
+
+// Create is http handler to create a board process.
+func (h *BoardHandler) Create(c echo.Context) error {
+	reqBoard := new(Board)
+	if err := c.Bind(reqBoard); err != nil {
+		return err
+	}
+
+	board := reqBoard.convertTo()
+	board.UserID = getUserIDFromToken(c)
+
+	b, err := h.intractor.Create(board)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resBoard := Board{}
+	resBoard.convertFrom(b)
+
+	return c.JSON(http.StatusCreated, resBoard)
+}
+
+// Update is http handler to update a board process.
+func (h *BoardHandler) Update(c echo.Context) error {
+	reqBoard := new(Board)
+	if err := c.Bind(reqBoard); err != nil {
+		return err
+	}
+	reqBoard.ID = c.Param("id")
+
+	board := reqBoard.convertTo()
+	board.UserID = getUserIDFromToken(c)
+
+	b, err := h.intractor.Update(board)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resBoard := Board{}
+	resBoard.convertFrom(b)
+
+	return c.JSON(http.StatusOK, resBoard)
+}
+
+// Delete is http handler to delete a board process.
+func (h *BoardHandler) Delete(c echo.Context) error {
+	reqBoard := new(Board)
+	if err := c.Bind(reqBoard); err != nil {
+		return err
+	}
+	reqBoard.ID = c.Param("id")
+
+	board := reqBoard.convertTo()
+	board.UserID = getUserIDFromToken(c)
+
+	err := h.intractor.Delete(board)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	return c.JSON(http.StatusNoContent, nil)
+}
+
+// Get is http handler to get user's board process.
+func (h *BoardHandler) Get(c echo.Context) error {
+	reqBoard := new(Board)
+	reqBoard.ID = c.Param("id")
+
+	board := reqBoard.convertTo()
+	board.UserID = getUserIDFromToken(c)
+
+	b, err := h.intractor.Get(board)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resBoard := Board{}
+	resBoard.convertFrom(b)
+
+	return c.JSON(http.StatusOK, resBoard)
+}
+
+// GetBoards is http handler to get user's boards process.
+func (h *BoardHandler) GetBoards(c echo.Context) error {
+	boards, err := h.intractor.GetBoards(model.User{ID: getUserIDFromToken(c)})
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resBoards := []Board{}
+	b := Board{}
+	for _, board := range boards {
+		b.convertFrom(board)
+		resBoards = append(resBoards, b)
+	}
+
+	return c.JSON(http.StatusOK, resBoards)
+}
