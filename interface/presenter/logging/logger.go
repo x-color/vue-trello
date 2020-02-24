5b8f4a0dfb80d851cabe462d157aece10daf508b
x-color <36035885+x-color@users.noreply.github.com>
2020-02-25 02:41:08 +0900

    Add comments

--- interface/presenter/logging/logger.go
+++ interface/presenter/logging/logger.go
@@ -5,18 +5,22 @@ import (
 	"io"
 )
 
+// Logger includes output interface.
 type Logger struct {
 	Output io.Writer
 }
 
+// Debug outputs debug log.
 func (l *Logger) Debug(msg string) {
 	fmt.Fprintf(l.Output, "[DEBUG] %s", msg)
 }
 
+// Info outputs info log.
 func (l *Logger) Info(msg string) {
 	fmt.Fprintf(l.Output, "[INFO] %s", msg)
 }
 
+// Error outputs error log.
 func (l *Logger) Error(msg string) {
 	fmt.Fprintf(l.Output, "[ERROR] %s", msg)
 }
