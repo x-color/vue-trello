131f2e8e58fa01399bd8eb6a9c57bda80f31d946
x-color <36035885+x-color@users.noreply.github.com>
2020-02-25 02:42:44 +0900

    Add logger generator

--- interface/presenter/logging/logger.go
+++ interface/presenter/logging/logger.go
@@ -7,20 +7,27 @@ import (
 
 // Logger includes output interface.
 type Logger struct {
-	Output io.Writer
+	output io.Writer
+}
+
+// NewLogger returns new Logger.
+func NewLogger(output io.Writer) Logger {
+	return Logger{
+		output: output,
+	}
 }
 
 // Debug outputs debug log.
 func (l *Logger) Debug(msg string) {
-	fmt.Fprintf(l.Output, "[DEBUG] %s", msg)
+	fmt.Fprintf(l.output, "[DEBUG] %s", msg)
 }
 
 // Info outputs info log.
 func (l *Logger) Info(msg string) {
-	fmt.Fprintf(l.Output, "[INFO] %s", msg)
+	fmt.Fprintf(l.output, "[INFO] %s", msg)
 }
 
 // Error outputs error log.
 func (l *Logger) Error(msg string) {
-	fmt.Fprintf(l.Output, "[ERROR] %s", msg)
+	fmt.Fprintf(l.output, "[ERROR] %s", msg)
 }
