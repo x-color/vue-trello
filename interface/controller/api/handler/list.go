dee7cf27a9bb0b2f62804bc86a66c96024c6107f
x-color <36035885+x-color@users.noreply.github.com>
2020-02-24 23:55:32 +0900

    Add http handler to control list

---
+++ interface/controller/api/handler/list.go
@@ -0,0 +1,107 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/x-color/vue-trello/model"
+	"github.com/x-color/vue-trello/usecase"
+)
+
+// List includes request data for List.
+type List struct {
+	ID      string `json:"id"`
+	BoardID string `json:"board_id"`
+	Title   string `json:"title"`
+}
+
+func (l *List) convertTo() model.List {
+	list := model.List{
+		ID:      l.ID,
+		BoardID: l.BoardID,
+		Title:   l.Title,
+	}
+
+	return list
+}
+
+func (l *List) convertFrom(list model.List) {
+	l.ID = list.ID
+	l.BoardID = list.BoardID
+	l.Title = list.Title
+}
+
+// ListHandler includes a interactor for List usecase.
+type ListHandler struct {
+	intractor usecase.ListUsecase
+}
+
+// NewListHandler returns a new ListHandler.
+func NewListHandler(l usecase.ListUsecase) ListHandler {
+	return ListHandler{
+		intractor: l,
+	}
+}
+
+// Create is http handler to create a list process.
+func (h *ListHandler) Create(c echo.Context) error {
+	reqList := new(List)
+	if err := c.Bind(reqList); err != nil {
+		return err
+	}
+	reqList.ID = c.Param("id")
+
+	list := reqList.convertTo()
+	list.UserID = getUserIDFromToken(c)
+
+	l, err := h.intractor.Create(list)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resList := List{}
+	resList.convertFrom(l)
+
+	return c.JSON(http.StatusOK, resList)
+}
+
+// Update is http handler to update a list process.
+func (h *ListHandler) Update(c echo.Context) error {
+	reqList := new(List)
+	if err := c.Bind(reqList); err != nil {
+		return err
+	}
+	reqList.ID = c.Param("id")
+
+	list := reqList.convertTo()
+	list.UserID = getUserIDFromToken(c)
+
+	l, err := h.intractor.Update(list)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	resList := List{}
+	resList.convertFrom(l)
+
+	return c.JSON(http.StatusOK, resList)
+}
+
+// Delete is http handler to delete a list process.
+func (h *ListHandler) Delete(c echo.Context) error {
+	reqList := new(List)
+	if err := c.Bind(reqList); err != nil {
+		return err
+	}
+	reqList.ID = c.Param("id")
+
+	list := reqList.convertTo()
+	list.UserID = getUserIDFromToken(c)
+
+	err := h.intractor.Delete(list)
+	if err != nil {
+		return convertToHTTPError(c, err)
+	}
+
+	return c.JSON(http.StatusNoContent, nil)
+}
