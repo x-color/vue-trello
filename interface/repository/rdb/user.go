20fbfa9b30f09e14d1be39e26255960ccd41a074
x-color <36035885+x-color@users.noreply.github.com>
2020-02-24 19:58:39 +0900

    Add user repositroy

---
+++ interface/repository/rdb/user.go
@@ -0,0 +1,77 @@
+package rdb
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/x-color/vue-trello/model"
+)
+
+// User is User data model for DB.
+type User struct {
+	ID   string `gorm:"primary_key"`
+	Name string
+	// Password is row. It will be hash.
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
+func (u *User) convertFrom(user model.User) {
+	u.ID = user.ID
+	u.Name = user.Name
+	u.Password = user.Password
+}
+
+func (u *User) convertTo() model.User {
+	user := model.User{
+		ID:       u.ID,
+		Name:     u.Name,
+		Password: u.Password,
+	}
+	return user
+}
+
+// Users is a slice of User data model.
+type Users []User
+
+// UserDBManager is DB manager for User.
+type UserDBManager struct {
+	db *gorm.DB
+}
+
+// Create registers a User to DB.
+func (m *UserDBManager) Create(user model.User) error {
+	u := User{}
+	u.convertFrom(user)
+
+	if err := m.db.Create(&u).Error; err != nil {
+		return model.ServerError{
+			Err: err,
+			ID:  u.ID,
+			Act: "create user",
+		}
+	}
+	return nil
+}
+
+// FindByName gets a User had specific name from DB.
+func (m *UserDBManager) FindByName(user model.User) (model.User, error) {
+	r := User{}
+	if err := m.db.Where(&User{Name: user.Name}).First(&r).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return model.User{}, model.NotFoundError{
+				Err: err,
+				ID:  "(No ID)",
+				Act: "find user",
+			}
+		}
+		return model.User{}, model.ServerError{
+			Err: err,
+			ID:  user.ID,
+			Act: "find user",
+		}
+	}
+	return r.convertTo(), nil
+}
