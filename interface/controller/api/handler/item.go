a033591daf69719390dae66d0fb481e5c4163204
x-color <36035885+x-color@users.noreply.github.com>
2020-02-24 23:27:10 +0900

    Add http handler for control item

---
+++ interface/controller/api/handler/item.go
@@ -0,0 +1,122 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/x-color/vue-trello/model"
+	"github.com/x-color/vue-trello/usecase"
+)
+
+// Item includes request data for Item.
+type Item struct {
+	ID     string   `json:"id"`
+	ListID string   `json:"list_id"`
+	Title  string   `json:"title"`
+	Text   string   `json:"text"`
+	Tags   []string `json:"tags"`
+}
+
+func (i *Item) convertTo() model.Item {
+	tags := model.Tags{}
+	for _, tagID := range i.Tags {
+		tags = append(tags, model.Tag{ID: tagID})
+	}
+
+	item := model.Item{
+		ID:     i.ID,
+		ListID: i.ListID,
+		Title:  i.Title,
+		Text:   i.Text,
+		Tags:   tags,
+	}
+
+	return item
+}
+
+func (i *Item) convertFrom(item model.Item) {
+	tags := []string{}
+	for _, tag := range item.Tags {
+		tags = append(tags, tag.ID)
+	}
+
+	i.ID = item.ID
+	i.ListID = item.ListID
+	i.Title = item.Title
+	i.Text = item.Text
+	i.Tags = tags
+}
+
+// ItemHandler includes a interactor for Item usecase.
+type ItemHandler struct {
+	intractor usecase.ItemUsecase
+}
+
+// NewItemHandler returns a new ItemHandler.
+func NewItemHandler(i usecase.ItemUsecase) *ItemHandler {
+	return &ItemHandler{
+		intractor: i,
+	}
+}
+
+// Create is http handler to create a item process.
+func (h *ItemHandler) Create(c echo.Context) error {
+	reqItem := new(Item)
+	if err := c.Bind(reqItem); err != nil {
+		return err
+	}
+
+	item := reqItem.convertTo()
+	item.UserID = getUserIDFromToken(c)
+
+	i, err := h.intractor.Create(item)
+	if err != nil {
+		return convertToHttpError(c, err)
+	}
+
+	resItem := Item{}
+	resItem.convertFrom(i)
+
+	return c.JSON(http.StatusCreated, resItem)
+}
+
+// Update is http handler to update a item process.
+func (h *ItemHandler) Update(c echo.Context) error {
+	reqItem := new(Item)
+	if err := c.Bind(reqItem); err != nil {
+		return err
+	}
+	reqItem.ID = c.Param("id")
+
+	item := reqItem.convertTo()
+	item.UserID = getUserIDFromToken(c)
+
+	i, err := h.intractor.Update(item)
+	if err != nil {
+		return convertToHttpError(c, err)
+	}
+
+	resItem := Item{}
+	resItem.convertFrom(i)
+
+	return c.JSON(http.StatusOK, resItem)
+}
+
+// Delete is http handler to delete a item process.
+func (h *ItemHandler) Delete(c echo.Context) error {
+	reqItem := new(Item)
+	if err := c.Bind(reqItem); err != nil {
+		return err
+	}
+	reqItem.ID = c.Param("id")
+
+	item := reqItem.convertTo()
+	item.UserID = getUserIDFromToken(c)
+
+	err := h.intractor.Delete(item)
+	if err != nil {
+		return convertToHttpError(c, err)
+	}
+
+	return c.JSON(http.StatusNoContent, nil)
+}
