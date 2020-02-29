aec5c796e3a2f186c4384029b7f052ea0bf9aebe
x-color <36035885+x-color@users.noreply.github.com>
2020-02-29 14:36:24 +0900

    Add tag resources to DB

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/x-color/vue-trello/interface/controller/api"
 	"github.com/x-color/vue-trello/interface/presenter/logging"
 	"github.com/x-color/vue-trello/interface/repository/rdb"
+	"github.com/x-color/vue-trello/model"
 	"github.com/x-color/vue-trello/usecase"
 )
 
@@ -17,6 +18,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	// Add init tags.
+	dbm.TagDBManager.Create(model.Tag{ID: "0", Name: "p1", Color: model.RED})
+	dbm.TagDBManager.Create(model.Tag{ID: "1", Name: "p2", Color: model.YELLOW})
+	dbm.TagDBManager.Create(model.Tag{ID: "2", Name: "p3", Color: model.GREEN})
+	dbm.TagDBManager.Create(model.Tag{ID: "3", Name: "p4", Color: model.BLUE})
 
 	location, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
