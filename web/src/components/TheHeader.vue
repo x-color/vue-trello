49b6c767bb1dfa68ad584efedbadd939c268a80e
x-color <36035885+x-color@users.noreply.github.com>
2020-03-01 13:12:21 +0900

    Fix format

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -1,46 +1,43 @@
 <template>
-    <v-app-bar :color="color" dark fixed app>
-      <router-link to="/boards" class="router-no-underline">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-toolbar-title class="diaplay-1 white--text" v-on="on">
-              <v-container>
-                <v-row justify="start">
-                  <v-col cols="auto" class="px-2">
-                    <v-img
-                      alt="Logo"
-                      src="@/assets/logo.png"
-                    />
-                  </v-col>
-                  <v-col cols="auto" class="px-0 pt-3">Vue Trello</v-col>
-                </v-row>
-              </v-container>
-            </v-toolbar-title>
-          </template>
-          <span>Home</span>
-        </v-tooltip>
-      </router-link>
-
-      <v-spacer />
-      <v-menu left bottom>
+  <v-app-bar :color="color" dark fixed app>
+    <router-link to="/boards" class="router-no-underline">
+      <v-tooltip bottom>
         <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-app-bar-nav-icon />
-          </v-btn>
+          <v-toolbar-title class="diaplay-1 white--text" v-on="on">
+            <v-container>
+              <v-row justify="start">
+                <v-col cols="auto" class="px-2">
+                  <v-img alt="Logo" src="@/assets/logo.png" />
+                </v-col>
+                <v-col cols="auto" class="px-0 pt-3">Vue Trello</v-col>
+              </v-row>
+            </v-container>
+          </v-toolbar-title>
         </template>
+        <span>Home</span>
+      </v-tooltip>
+    </router-link>
 
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in menu"
-            :key="index"
-            :disabled="!item.active"
-            @click="item.action"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+    <v-spacer />
+    <v-menu left bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-app-bar-nav-icon />
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in menu"
+          :key="index"
+          :disabled="!item.active"
+          @click="item.action"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
 
 <script>
