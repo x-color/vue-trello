410dbc85e55020cb337b05f771fc919500427f59
x-color <36035885+x-color@users.noreply.github.com>
2020-03-03 00:59:47 +0900

    Add test code for api moving list

---
+++ tests/move_list.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -eu
+
+
+curl -s -X POST localhost:8080/signup -H 'Content-Type:application/json; charset=UTF-8' -d '{"name":"testuser", "password":"pass"}'
+
+curl -s -X POST localhost:8080/signin -H 'Content-Type:application/json; charset=UTF-8' -d '{"name":"testuser", "password":"pass"}' -c /tmp/cookie.file
+
+curl -s localhost:8080/api/resources -H 'X-XSRF-TOKEN:csrf' -H 'Content-Type:application/json; charset=UTF-8' -b /tmp/cookie.file
+
+curl -s localhost:8080/api/boards -H 'X-XSRF-TOKEN:csrf' -H 'Content-Type:application/json; charset=UTF-8' -b /tmp/cookie.file
+
+# Create
+
+curl -s -X POST localhost:8080/api/boards \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"title": "first", "color":"red"}' \
+-b /tmp/cookie.file \
+| tee /tmp/tmp.file
+
+BID1=$(cat /tmp/tmp.file | tail -1 | jq .id -r)
+
+# Create lists
+curl -s -X POST localhost:8080/api/lists \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"board_id":"'$BID1'", "title": "first_list"}' \
+-b /tmp/cookie.file \
+| tee /tmp/tmp.file
+
+LID1=$(cat /tmp/tmp.file | tail -1 | jq .id -r)
+
+curl -s -X POST localhost:8080/api/lists \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"board_id":"'$BID1'", "title": "second_list"}' \
+-b /tmp/cookie.file \
+| tee /tmp/tmp.file
+
+LID2=$(cat /tmp/tmp.file | tail -1 | jq .id -r)
+
+curl -s -X POST localhost:8080/api/lists \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"board_id":"'$BID1'", "title": "first_list"}' \
+-b /tmp/cookie.file \
+| tee /tmp/tmp.file
+
+LID3=$(cat /tmp/tmp.file | tail -1 | jq .id -r)
+
+# Create items
+curl -s -X POST localhost:8080/api/items \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"list_id":"'$LID1'", "title": "first_item", "text": "hahaha", "tags":[]}' \
+-b /tmp/cookie.file \
+
+curl -s -X POST localhost:8080/api/items \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"list_id":"'$LID2'", "title": "fourth_item", "text": "hahaha", "tags":[]}' \
+-b /tmp/cookie.file \
+
+curl -s -X POST localhost:8080/api/items \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"list_id":"'$LID3'", "title": "first_item", "text": "hahaha", "tags":[]}' \
+-b /tmp/cookie.file \
+
+
+# Show
+
+echo 
+echo "## Show ##"
+echo 
+
+curl -s localhost:8080/api/boards/$BID1 \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-b /tmp/cookie.file \
+| jq .
+
+# Move
+
+curl -s -X PATCH localhost:8080/api/lists/$LID2/move \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-d '{"board_id":"'$BID1'", "before": "'$LID3'"}' \
+-b /tmp/cookie.file \
+
+echo 
+echo "## Moved ##"
+echo 
+
+curl -s localhost:8080/api/boards/$BID1 \
+-H 'X-XSRF-TOKEN:csrf' \
+-H 'Content-Type:application/json; charset=UTF-8' \
+-b /tmp/cookie.file \
+| jq .
